57aa00f741709fafd63c7b75a3c2747f7f7faf61
indynames5 <indynames@gmail.com>
2019-12-04 21:46:55 +0700

    Try to create recyclerview

--- app/src/main/java/com/example/uberfood/MainActivity.java
+++ app/src/main/java/com/example/uberfood/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.uberfood;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -15,7 +14,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
@@ -23,15 +21,12 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth  = FirebaseAuth.getInstance();
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    RecyclerView recyclerView;
     ArrayList<Menu> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
-        recyclerView = (RecyclerView) findViewById(R.id.myRecycle_view);
 
 
     }
@@ -61,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
                                 updateUI(null);
                             }
 
-
+                            // ...
                         }
                     });
 
--- app/src/main/java/com/example/uberfood/MyAdapter.java
+++ app/src/main/java/com/example/uberfood/MyAdapter.java
@@ -22,68 +22,69 @@ import java.util.ArrayList;
 
 import static com.squareup.picasso.Picasso.*;
 
-//public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
-//    private Context context;
-//    private String[] id;
-//    private String[] image;
-//    private  String[] name;
-//    private String[] rate;
-//
-//    private OnNoteListener mOnNoteListener;
-//
-//    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-//        TextView nametext;
-//        TextView ratetext;
-//        ImageView imageView;
-//        OnNoteListener onNoteListener;
-//
-//        public MyViewHolder(View itemView,OnNoteListener onNoteListener) {
-//            super(itemView);
-//            this.nametext = (TextView) itemView.findViewById(R.id.nametext);
-//            this.ratetext = (TextView) itemView.findViewById(R.id.ratetext);
-//            this.imageView = (ImageView) itemView.findViewById(R.id.imageView);
-//            this. onNoteListener = onNoteListener ;
-//            itemView.setOnClickListener(this);
-//        }
-//
-//        @Override
-//        public void onClick(View v) {
-//            onNoteListener.onNoteClick(getAdapterPosition());
-//        }
-//
-//
-//    }
-//
-////    public CustomAdepter(Context mContext, String[] image,String[] title,String[] rate,OnNoteListener onNoteListener) {
-////        this.context = mContext;
-////        this.image = image;
-////        this.name = title;
-////        this.rate = rate;
-////        this.mOnNoteListener = onNoteListener;
-////    }
-//
-//    @Override
-//    public MyViewHolder onCreateViewHolder(ViewGroup parent,
-//                                           int viewType) {
-//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview, parent, false);
-//        MyViewHolder myViewHolder = new MyViewHolder(view,mOnNoteListener);
-//        return myViewHolder;
-//    }
-//
-////    @Override
-////    public void onBindViewHolder (final MyViewHolder holder, final int i) {
-////        holder.nametext.setText(name[i]);
-////        holder.ratetext.setText(rate[i]);
-////        with(context).load(image[i]).into(holder.imageView);
-////    }
-//    @Override
-//    public int getItemCount() {
-//        return name.length;
-//    }
-//
-//    public interface OnNoteListener{
-//        void onNoteClick(int position);
-//    }
-//
-//
-//}
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
+    private Context context;
+    private String[] id;
+    private String[] image;
+    private  String[] name;
+    private String[] rate;
+
+    private OnNoteListener mOnNoteListener;
+
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView nametext;
+        TextView ratetext;
+        ImageView imageView;
+        OnNoteListener onNoteListener;
+
+        public MyViewHolder(View itemView,OnNoteListener onNoteListener) {
+            super(itemView);
+            this.nametext = (TextView) itemView.findViewById(R.id.nametext);
+            this.ratetext = (TextView) itemView.findViewById(R.id.ratetext);
+            this.imageView = (ImageView) itemView.findViewById(R.id.imageView);
+            this. onNoteListener = onNoteListener ;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            onNoteListener.onNoteClick(getAdapterPosition());
+        }
+
+
+    }
+
+    public CustomAdepter(Context mContext, String[] image,String[] title,String[] rate,OnNoteListener onNoteListener) {
+        this.context = mContext;
+        this.image = image;
+        this.name = title;
+        this.rate = rate;
+        this.mOnNoteListener = onNoteListener;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                           int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview, parent, false);
+        MyViewHolder myViewHolder = new MyViewHolder(view,mOnNoteListener);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder (final MyViewHolder holder, final int i) {
+        holder.nametext.setText(name[i]);
+        holder.ratetext.setText(rate[i]);
+        with(context).load(image[i]).into(holder.imageView);
+    }
+    @Override
+    public int getItemCount() {
+        return name.length;
+    }
+
+    public interface OnNoteListener{
+        void onNoteClick(int position);
+    }
+
+
+}
